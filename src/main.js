b18f19e31b7765cb4c8b40938f977d733bd127f5
kartikayt <kartikayt7@gmail.com>
2023-07-20 23:30:23 +0530

    env setup attempt

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './style.css';
 // import { APP_URL } from "../data";
 
 const APP_URL = import.meta.env.VITE_APP_URL;
-console.log(APP_URL);
 
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("accessToken")) {
